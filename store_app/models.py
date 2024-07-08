0152d541a70a09da217f46d6687ea1aae0016de5
Siyavash Feyzi <feyzisiyavash@gmail.com>
2024-07-08 16:00:37 +0330

    Create UserInfo, Payment and Address empty model class

--- store_app/models.py
+++ store_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserInfo(models.Model):
+    """_summary_
+
+    Args:
+        models (_type_): _description_
+    """
+    pass
+
+
+class Payment(models.Model):
+    """_summary_
+
+    Args:
+        models (_type_): _description_
+    """
+    pass
+
+
+class Address(models.Model):
+    """_summary_
+
+    Args:
+        models (_type_): _description_
+    """
+    pass
