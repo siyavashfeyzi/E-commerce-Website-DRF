4fd57befb35c639296c7d48b1ae24b4cb5a05185
Siyavash Feyzi <feyzisiyavash@gmail.com>
2024-07-10 23:56:36 +0330

    Create User , Address and Payment Model

--- store_app/models.py
+++ store_app/models.py
@@ -1,15 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 # Create your models here.
 
 
-class UserInfo(models.Model):
-    """_summary_
+class MyUserManager(BaseUserManager):
 
-    Args:
-        models (_type_): _description_
-    """
-    pass
+    def create_user(self, name, email, password=None):
+        """
+            Create and save a user with the given name and email.
+        """
+        if not email:
+            raise ValueError("User must have an email address.")
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    objects = MyUserManager()
+
+    USERNAME_FIELD = "email"
 
 
 class Payment(models.Model):
